70d41eae57dfe5a9a50b8fad690bb57a8974aa1a
Javier Gómez <javiergomezo@protonmail.com>
2023-04-29 11:24:32 +0200

    feat(nvim): add mason autoinstall plugins

--- nvim/.config/nvim/lua/plugins/mason.lua
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -1,26 +1,42 @@
 return {
   "williamboman/mason.nvim",
   cmd = {"Mason", "MasonInstall"},
+  keys = { { "<leader>cm", "<cmd>Mason<cr>", desc = "Mason" } },
   dependencies = {
     "williamboman/mason-lspconfig.nvim",
     "neovim/nvim-lspconfig",
   },
   build = ":MasonUpdate",
-  config = function()
-    require("mason").setup {
-      ensure_installed = {
-        "lua-language-server",
-        "flake8",
-        "black",
-        "eslint_d"
-      },
-      ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
+  opts = {
+    ensure_installed = {
+      "lua-language-server",
+      "flake8",
+      "black",
+      "eslint_d"
+    },
+    ui = {
+      icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
       }
     }
+  },
+  config = function(_, opts)
+    require('mason').setup(opts)
+    local mr = require("mason-registry")
+    local function ensure_installed()
+      for _, tool in ipairs(opts.ensure_installed) do
+        local p = mr.get_package(tool)
+        if not p:is_installed() then
+          p:install()
+        end
+      end
+    end
+    if mr.refresh then
+      mr.refresh(ensure_installed)
+    else
+      ensure_installed()
+    end
   end
 }

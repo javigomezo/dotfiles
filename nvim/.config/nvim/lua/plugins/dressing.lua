c50531fc63475e65ec4556408fae6d95dcde4104
javigomezo <javiergomezo@protonmail.com>
2023-08-30 11:32:18 +0200

    fix(nvim): commented out anchor

--- nvim/.config/nvim/lua/plugins/dressing.lua
+++ nvim/.config/nvim/lua/plugins/dressing.lua
@@ -21,7 +21,7 @@ return {
       start_in_insert = true,
 
       -- These are passed to nvim_open_win
-      anchor = "SW",
+      -- anchor = "SW",
       border = "rounded",
       -- 'editor' and 'win' will default to being centered
       relative = "cursor",
@@ -117,7 +117,7 @@ return {
     -- Options for built-in selector
     builtin = {
       -- These are passed to nvim_open_win
-      anchor = "NW",
+      -- anchor = "NW",
       border = "rounded",
       -- 'editor' and 'win' will default to being centered
       relative = "editor",

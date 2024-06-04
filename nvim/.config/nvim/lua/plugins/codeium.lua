4f12d71609774b69a5d368ef4974e7664e2f8389
Javier Gomez Ortiz <javier.gomez@eviden.com>
2024-06-04 12:21:23 +0000

    fix(nvim): fix codeium plugin to run in nvim 0.10

--- nvim/.config/nvim/lua/plugins/codeium.lua
+++ nvim/.config/nvim/lua/plugins/codeium.lua
@@ -1,4 +1,13 @@
 return {
   "Exafunction/codeium.vim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "hrsh7th/nvim-cmp"
+  },
+  cmd = "Codeium",
+  build = ":Codeium Auth",
+  opts = {},
   event = "BufEnter",
+  config = function()
+  end,
 }
